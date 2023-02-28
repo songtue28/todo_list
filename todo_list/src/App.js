e0a13c5b1838a4e508e1663ebc3d7297183d5113
songtue28 <songtue28@gmail.com>
2023-03-01 01:28:43 +0700

    add-fooder/header

---
+++ todo_list/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function Header() {
+  return (
+    <div className="header">Header</div>
+  )
+}
+function Footer() {
+  return (
+    <div className="header">Footer</div>
+  )
+}
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      Hello world
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
